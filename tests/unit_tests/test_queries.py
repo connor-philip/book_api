56d0157940e9af0f2278e7ed083fb9f28d36c532
connor-philip <connorphilip12@hotmail.com>
2019-02-10 21:06:06 +0000

    Adding unit test outlines for the functions

---
+++ tests/unit_tests/test_queries.py
@@ -0,0 +1,21 @@
+import unittest
+import queries
+
+
+class TestDoesBookExist(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_false_returned_when_book_does_not_exist(self):
+        pass
+
+    def test_true_returned_when_book_does_exist(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
