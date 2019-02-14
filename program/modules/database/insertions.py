1c0e808d450a3aedc2b0d5eeb146d6e597a0d24c
connor-philip <connorphilip12@hotmail.com>
2019-02-14 22:07:37 +0000

    Filling out function

--- program/modules/database/insertions.py
+++ program/modules/database/insertions.py
@@ -1,14 +1,44 @@
-from db_connection import db
-from hashlib import md5
 
 
 class AddBook:
 
-    def __init__(self, ):
+    def __init__(self):
         pass
 
-    def check_if_book_dict_has_valid_values(self, newBookDict):
-        pass
+    def format_type_error_message(self, description, expectedType, incorrectValue):
+        actualType = type(incorrectValue)
+        errorMessage = "\n{} is not a {}:\n\t{}\nInstead type is: {}".format(description,
+                                                                             expectedType,
+                                                                             incorrectValue,
+                                                                             actualType)
+        return errorMessage
+
+    def check_if_book_dict_has_valid_values(self, title, genres, authors=[], tags=[], isbn10="", isbn13=""):
+        errorMessage = ""
+
+        # Check parameter types
+        for strParam in [title, isbn10, isbn13]:
+            if isinstance(strParam, str) is False:
+                errorMessageLine = self.format_type_error_message(strParam, "str", strParam)
+                errorMessage = "{}\n{}".format(errorMessage, errorMessageLine)
+
+        for listParam in [genres, authors, tags]:
+            if isinstance(listParam, list):
+                for strItem in listParam:
+                    if isinstance(strItem, str) is False:
+                        description = "{} list item".format(listParam)
+                        errorMessageLine = self.format_type_error_message(description, "str", strItem)
+                        errorMessage = "{}\n{}".format(errorMessage, errorMessageLine)
+            else:
+                errorMessageLine = self.format_type_error_message(listParam, "list", listParam)
+                errorMessage = "{}\n{}".format(errorMessage, errorMessageLine)
+
+        # If error message is empty return True
+        if errorMessage:
+            print(errorMessage)
+            return False
+        else:
+            return True
 
     def create_new_book_dict_with_selected_keys(self, newBookDict):
         pass
