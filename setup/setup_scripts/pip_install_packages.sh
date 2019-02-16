d420df8098e842907a45d4f345dc049b443027e0
connor-philip <connorphilip12@hotmail.com>
2019-02-16 16:25:48 +0000

    Adding local py modules to pip install

--- setup/setup_scripts/pip_install_packages.sh
+++ setup/setup_scripts/pip_install_packages.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+SETUP_SCRIPTS_DIR="${0%/*}"
+SETUP_DIR=$CONTAINER_SCRIPTS_DIR/..
+BOOK_API_DIR=$SETUP_DIR/..
+
+cd $BOOK_API_DIR
+
+pip3 install .
 pip3 install pymongo
 pip3 install flask
+
+cd -
