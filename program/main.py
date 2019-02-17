0c7d731312fa09524bb86b667b8f15c8fbb33459
connor-philip <connorphilip12@hotmail.com>
2019-02-17 00:48:36 +0000

    Adding API endpoint for books

--- program/main.py
+++ program/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-app = Flask("book_api")
+from flask import Flask, jsonify, render_template
+import book_api.modules.database.queries as queries
+app = Flask("book_api_app")
 
 
 @app.route("/", methods=['GET'])
@@ -7,3 +8,11 @@ app = Flask("book_api")
 @app.route("/home/", methods=['GET'])
 def home():
     return render_template("home_template.html")
+
+
+@app.route("/api/books/", methods=['GET'])
+def get_books():
+    cursorObject = queries.get_all_books()
+    booksJson = jsonify(list(cursorObject))
+
+    return booksJson
--- program/modules/database/queries.py
+++ program/modules/database/queries.py
@@ -3,3 +3,7 @@ from book_api.modules.database.db_connection import bookdb as bookdb
 
 def get_book_dict_by_id(_id):
     return bookdb.books.find_one({"_id": _id})
+
+
+def get_all_books():
+    return bookdb.books.find({})
