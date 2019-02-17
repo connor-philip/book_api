df34b0637c97811bd179137ae889d472ec7adce8
connor-philip <connorphilip12@hotmail.com>
2019-02-17 01:08:06 +0000

    Adding tests for function

--- tests/unit_tests/test_database/test_queries.py
+++ tests/unit_tests/test_database/test_queries.py
@@ -2,6 +2,7 @@ import unittest
 import book_api.modules.database.queries as queries
 import book_api.modules.database.insertions as insertions
 from book_api.modules.database.db_connection import client
+from pymongo.cursor import Cursor
 
 
 class TestGetBookDictById(unittest.TestCase):
@@ -22,6 +23,42 @@ class TestGetBookDictById(unittest.TestCase):
         returnedValue = queries.get_book_dict_by_id("nonExistantDict")
 
         self.assertIsNone(returnedValue)
+
+
+class TestGetAllBooks(unittest.TestCase):
+
+    def setUp(self):
+        insertions.AddBook("title1", ["author1"])
+
+    def tearDown(self):
+        client.drop_database("bookdb")
+
+    def test_cursor_obj_returned(self):
+        returnedValue = queries.get_all_books()
+
+        self.assertIsInstance(returnedValue, Cursor)
+
+    def test_all_books_found(self):
+        insertions.AddBook("title2", ["author2"])
+
+        returnedValue = queries.get_all_books()
+        returnedValueLength = len(list(returnedValue))
+        expectedValue = 2
+
+        self.assertEqual(expectedValue, returnedValueLength)
+
+    def test_full_book_dict_in_cursor(self):
+        returnedValue = queries.get_all_books()
+        returnedValueDict = returnedValue[0]
+        expectedValue = {"authors": ["author1"],
+                         "_id": "3f5468de0bfbe111586f7649a3c8d115",
+                         "genres": [],
+                         "isbn13": "",
+                         "isbn10": "",
+                         "title": "title1",
+                         "tags": []}
+
+        self.assertEqual(expectedValue, returnedValueDict)
 
 
 if __name__ == "__main__":
