0122483c869ec3805791d59967da7815e4e16c19
connor-philip <connorphilip12@hotmail.com>
2019-03-10 19:00:03 +0000

    changing paths

--- environment/unit_test_container/run_unit_test_container.sh
+++ environment/unit_test_container/run_unit_test_container.sh
@@ -3,11 +3,11 @@
 UNIT_TEST_CONTAINER_dir="${0%/*}"
 ENVIRONMENT_DIR=$UNIT_TEST_CONTAINER_dir/..
 BOOK_API_DIR=$ENVIRONMENT_DIR/..
-UNIT_TEST_DIR=$BOOK_API_DIR/tests/unit/tests
 
 cd $BOOK_API_DIR
-ABSOLUTE_UNIT_TEST_DIR=$(pwd)
+ABSOLUTE_BOOK_API_DIR=$(pwd)
 
-docker run --net booknet --label book_api_unit_tests_container --volume $ABSOLUTE_UNIT_TEST_DIR/:/app book_api_unit_tests
+
+docker run --net booknet --label book_api_unit_tests_container --volume $ABSOLUTE_BOOK_API_DIR/:/app book_api_unit_tests
 
 cd -
