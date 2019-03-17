8f593170691c95707b20491b05ea28caa9ff29ea
connor-philip <connorphilip12@hotmail.com>
2019-03-17 20:46:08 +0000

    Adding options to kill test variants of the containers

--- environment/setup_scripts/kill_remove_container.sh
+++ environment/setup_scripts/kill_remove_container.sh
@@ -6,12 +6,18 @@ fullContainerName=book_api_$container
 if [[ $container == "app" ]]; then
     docker ps | grep book_api_app && docker kill $fullContainerName
     docker ps -a | grep book_api_app && docker rm $fullContainerName
+elif [[ $container == "test_app" ]]; then
+    docker ps | grep book_api_test_app && docker kill $fullContainerName
+    docker ps -a | grep book_api_test_app && docker rm $fullContainerName
 elif [[ $container == "db" ]]; then
     docker ps | grep book_api_db && docker kill $fullContainerName
     docker ps -a | grep book_api_db && docker rm $fullContainerName
+elif [[ $container == "test_db" ]]; then
+    docker ps | grep book_api_test_db && docker kill $fullContainerName
+    docker ps -a | grep book_api_test_db && docker rm $fullContainerName
 else
     echo "Invalid parameter supplied."
     echo "Run the script with adding either app or db as an argument"
-    echo "e.g. bash kill_remove_container app"
+    echo "e.g. sudo bash kill_remove_container app"
     exit 1
 fi
